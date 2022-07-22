4aa98d3ee18c04ae82e322e56114e2a551cb3995
metamodernity <95966082+metamodernity@users.noreply.github.com>
2022-07-22 13:38:44 +0400

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class HelloWorld {
+  public static void main(string[] args){
+      Data now = new Date();
+      System.out.println("Hello World!");
+      System.out.println("now: " + now);
+    }
+}
